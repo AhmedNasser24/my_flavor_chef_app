24a8446dc5bc89dfbdd7cac9ca3f362224b45251
AhmedNasser24 <https://github.com/AhmedNasser24>
2025-11-11 11:06:29 +0200

    add font family

---
+++ lib/core/utils/themes/get_theme_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData getThemeData() {
+  return ThemeData(
+    fontFamily: "Cairo",
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  );
+}
