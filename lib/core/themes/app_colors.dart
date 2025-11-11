e9f9aa82048857cd16ee36dfd7277c882259466b
AhmedNasser24 <https://github.com/AhmedNasser24>
2025-11-11 10:55:19 +0200

    add app colors

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFAF8E42);
+  static const Color background = Color(0xFF0C0C0C);
+  static const Color white = Color(0xFFF4F4F5);
+  static const Color black11 = Color(0xFF1D1A11);
+}
