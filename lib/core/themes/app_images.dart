ae59426e8cab05306bcd08986e626711b9b4c020
AhmedNasser24 <https://github.com/AhmedNasser24>
2025-11-11 10:45:49 +0200

    add_images

---
+++ lib/core/themes/app_images.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+
+  static const String imagesBackArrow = "assets/images/back_arrow.png";
+
+  static const String imagesConnectWithUsIcon = "assets/images/connect_with_us_icon.png";
+
+  static const String imagesFacebook = "assets/images/Facebook.png";
+
+  static const String imagesForwardIosArrow = "assets/images/forward_ios_arrow.png";
+
+  static const String imagesInstagram = "assets/images/instagram.png";
+
+  static const String imagesInvisibleEyeIcon = "assets/images/invisible_eye_icon.png";
+
+  static const String imagesLocationIcon = "assets/images/location_icon.png";
+
+  static const String imagesLockIcon = "assets/images/lock_icon.png";
+
+  static const String imagesLogoutIcon = "assets/images/logout_icon.png";
+
+  static const String imagesNotificationIcon = "assets/images/notification_icon.png";
+
+  static const String imagesPersonIcon = "assets/images/person_icon.png";
+
+  static const String imagesPhoneIcon = "assets/images/phone_icon.png";
+
+  static const String imagesProfile1Icon = "assets/images/profile1_icon.png";
+
+  static const String imagesProfileIcon = "assets/images/profile_icon.png";
+
+  static const String imagesSettingIcon = "assets/images/setting_icon.png";
+
+  static const String imagesSnapchat = "assets/images/snapchat.png";
+
+  static const String imagesSplashLogo = "assets/images/splash_logo.png";
+
+  static const String imagesTiktok = "assets/images/tiktok.png";
+
+  static const String imagesVisibleEyeIcon = "assets/images/visible_eye_icon.png";
+
+  static const String imagesWhatsapp1Icon = "assets/images/whatsapp1_icon.png";
+
+  static const String imagesWhatsapp2Icon = "assets/images/whatsapp2_icon.png";
+}
+
